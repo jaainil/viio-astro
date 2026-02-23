cd69cfe6fa168571729263827d8e3ba6364b96de
Jainil Prajapati <github-jainil@1209.in>
2026-02-24 02:03:58 +0530

    feat: enhance SEO and add sitemap support
    - Updated package.json to include new dependencies for SEO and sitemap generation.
    - Added robots.txt file to allow search engines to index the site and specify the sitemap location.
    - Enhanced Layout.astro to integrate SEO metadata using astro-seo and structured data for better search visibility.
    - Updated descriptions in various pages (About, Contact, Index, Services) to improve SEO and provide clearer information about services offered.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import llmsTxtIntegration from "astro-llms-txt-generator";
 
 export default defineConfig({
+  site: "https://shreecreatixagency.com",
+  integrations: [sitemap(), robotsTxt(), llmsTxtIntegration()],
   server: {
     host: process.env.HOST ?? "0.0.0.0",
     port: parseInt(process.env.PORT || "4321"),
